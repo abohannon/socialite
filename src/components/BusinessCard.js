3af2e1c255c9e74801d782b40c65c7efd64fbaa9
Adam Bohannon <abo46n2@gmail.com>
2018-04-11 15:07:07 -0700

    Finished 1st iteration

--- src/components/BusinessCard.js
+++ src/components/BusinessCard.js
@@ -2,8 +2,41 @@ import React from 'react';
 import { View, Text, Image } from 'react-native';
 import { Card } from 'react-native-elements';
 import { Button } from './common';
+import { GREY_LIGHT, GREY_DARK, WHITE } from '../constants/style';
 
-renderCategories = ({ categories }) => categories.map(category => category.title).join(', ');
+const styles = {
+  containerStyle: {
+    marginBottom: 16,
+  },
+  contentStyle: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginBottom: 8,
+  },
+  addressContainerStyle: {
+    flex: 2,
+    paddingRight: 8,
+  },
+  addressTextStyle: {
+    fontSize: 18,
+  },
+  ratingsContainerStyle: {
+    flex: 1,
+  },
+  ratingsStyle: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  categoriesStyle: {
+    fontSize: 12,
+    color: GREY_LIGHT,
+  },
+};
+
+
+const renderCategories = ({ categories }) => categories.map(category => category.title).join(', ');
 
 const BusinessCard = (props) => {
   const {
@@ -13,31 +46,47 @@ const BusinessCard = (props) => {
     reviewCount,
     location,
   } = props;
+
+  const {
+    containerStyle,
+    contentStyle,
+    addressContainerStyle,
+    ratingsContainerStyle,
+    ratingsStyle,
+    addressTextStyle,
+    categoriesStyle,
+  } = styles;
+
   return (
     <Card
       image={{ uri: imageUri }}
       title={name}
+      titleStyle={{ color: GREY_DARK }}
     >
-      <View className="card__content">
-        <View>
-          <Text>
-            Rating: {rating}
-          </Text>
-          <Text>
-            Reviews: {reviewCount}
-          </Text>
+      <View style={containerStyle}>
+        <View className="card__content" style={contentStyle}>
+          <View style={addressContainerStyle}>
+            <Text style={addressTextStyle}>
+              {location.display_address[0]}
+            </Text>
+            <Text style={addressTextStyle}>
+              {location.display_address[1]}
+            </Text>
+          </View>
+          <View className="card__ratings" style={ratingsContainerStyle}>
+            <View style={ratingsStyle}>
+              <Text>Rating:</Text>
+              <Text>{rating}</Text>
+            </View>
+            <View style={ratingsStyle}>
+              <Text>Reviews:</Text>
+              <Text>{reviewCount}</Text>
+            </View>
+          </View>
         </View>
         <View>
-          <Text>
-            {location.display_address[0]}
-          </Text>
-          <Text>
-            {location.display_address[1]}
-          </Text>
-        </View>
-        <View>
-          <Text>
-            {this.renderCategories(props)}
+          <Text style={categoriesStyle}>
+            {renderCategories(props)}
           </Text>
         </View>
       </View>
--- src/constants/style.js
+++ src/constants/style.js
@@ -1,2 +1,5 @@
 export const WHITE = '#fff';
 export const BLACK = '#000';
+export const GREY_LIGHT = '#6e6e6e';
+export const GREY_MEDIUM = '#4a4a4a';
+export const GREY_DARK = '#222';
